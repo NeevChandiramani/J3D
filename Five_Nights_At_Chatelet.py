4c96ecf75d11a9c1d9b0fea14a15a5508f35b150
Maaaxxxxxxxx <168303233+Maaaxxxxxxxx@users.noreply.github.com>
2025-11-06 10:26:40 +0100

    Implement player movement controls
    Added movement function for player character.

--- Five_Nights_At_Chatelet.py
+++ Five_Nights_At_Chatelet.py
@@ -15,12 +15,21 @@ joueur = Entity(
     scale_y = 3,
     collider = 'box')
 
-
 camera.parent = joueur
 camera.position = (0, 2, -6)
 camera.rotation_x = 15
 camera.fov = 90
 camera.clip_plane_near = 0.1
 camera.clip_plane_far = 100
+
+vitesse = 6.7
+def mouvement() :
+    avance = joueur.forward * (held_keys['w'])
+    recule = joueur.forward * (- held_keys['s'])    #forward et right sont de type vecteur, pour ça qu'on multiplie
+    droite = joueur.right * (held_keys['d'])
+    gauche = joueur.right * (- held_keys['a'])    #touche de qwerty mais ça marche en azerty
+    direction = (avance + recule + droite + gauche).normalized() * time.dt * 6.7
+    joueur.position += direction
+
 
 Five_nights_at_chatelet.run()
