b3393a5e8dd5b9b40ae0efb9e43b934125008be9
Maaaxxxxxxxx <168303233+Maaaxxxxxxxx@users.noreply.github.com>
2025-11-06 09:53:40 +0100

    Set up Ursina app with ground entity
    Initialize Ursina application and create a ground entity.

--- Five_Nights_At_Chatelet.py
+++ Five_Nights_At_Chatelet.py
@@ -1,1 +1,12 @@
+from ursina import *
+#pip install ursina on oublie pas tu connais
 
+Five_nights_at_chatelet = Ursina()
+
+sol = Entity(
+    model='plane',
+    texture='shore',
+    collider='box',
+    scale=(50, 1, 50))
+
+Five_nights_at_chatelet.run()
