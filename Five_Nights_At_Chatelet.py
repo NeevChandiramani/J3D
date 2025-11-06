8e40b0a0f09386a2401705c2300844cf3c7ee9a5
Maaaxxxxxxxx <168303233+Maaaxxxxxxxx@users.noreply.github.com>
2025-11-06 10:14:38 +0100

    Implement camera control for player entity
    Added camera control to the player entity.

--- Five_Nights_At_Chatelet.py
+++ Five_Nights_At_Chatelet.py
@@ -10,10 +10,11 @@ sol = Entity(
     scale = (50, 1, 50))
 
 joueur = Entity(
-    model = 'cube',
+    model = 'cube',    #Pour avoir d'autres model il faut télécharger des trucs sinon ça marche pas
     color = color.red,
     scale_y = 3,
     collider = 'box')
+
 
 camera.parent = joueur
 camera.position = (0, 2, -6)
