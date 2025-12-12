8aa89a7a561b85a14d31a66887ef9004929810b1
Zendorf7 <maxime77.gontran@gmail.com>
2025-12-12 18:35:21 +0100

    Update Five_Nights_At_Chatelet.py
    Stam

--- Five_Nights_At_Chatelet.py
+++ Five_Nights_At_Chatelet.py
@@ -53,6 +53,23 @@ test_cube = Entity(
     shader=lit_with_shadows_shader
 )
 
+
+max_stamina = 100
+current_stamina = max_stamina
+stamina_drain_rate = 25 
+stamina_regen_rate = 15  
+sprint_speed_multiplier = 2.0
+base_speed = 6.7
+
+# Stamina UI (à modifier)
+stamina_text = Text(
+    text='Stamina: 100',
+    position=(-0.8, -0.45),
+    scale=1.5,
+    parent=camera.ui,
+    color=color.white
+)
+
 mouse.locked = True
 mouse.visible = False 
 
@@ -67,12 +84,39 @@ def mouvement_camera():
     camera_pivot.rotation_x = clamp(camera_pivot.rotation_x, -30, 45)
     camera.position = (0, 0, -5)
 
-def mouvement_joueur() :
+def mouvement_joueur():
+    global current_stamina
+    
+    is_moving = held_keys['w'] or held_keys['s'] or held_keys['a'] or held_keys['d']
+    is_sprinting = held_keys['shift'] and current_stamina > 1 and is_moving
+
+    if is_sprinting:
+
+        current_stamina -= stamina_drain_rate * time.dt
+        current_stamina = max(0, current_stamina)  
+        
+        
+        if current_stamina > 0:
+            current_speed = base_speed * sprint_speed_multiplier
+        else:
+            current_speed = base_speed
+        
+    else:
+        current_speed = base_speed
+        
+        if current_stamina < max_stamina:
+            current_stamina += stamina_regen_rate * time.dt
+            current_stamina = min(max_stamina, current_stamina)  
+    
+    
+    stamina_text.text = f'Stamina: {int(current_stamina)}'
+    
+    
     avance = Vec3(camera_pivot.forward.x, 0, camera_pivot.forward.z) * held_keys['w']
     recule = Vec3(camera_pivot.forward.x, 0, camera_pivot.forward.z) * -held_keys['s']
     droite = Vec3(camera_pivot.right.x, 0, camera_pivot.right.z) * held_keys['d']
     gauche = Vec3(camera_pivot.right.x, 0, camera_pivot.right.z) * -held_keys['a']   #touche de qwerty mais ça marche en azerty
-    direction = (avance + recule + droite + gauche).normalized() * time.dt * 6.7
+    direction = (avance + recule + droite + gauche).normalized() * time.dt * current_speed
     joueur.position += direction
 
 def input(key):
