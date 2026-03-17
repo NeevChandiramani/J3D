1f8b59a0bc9489380438b731c791076389178e33
shin697 <maxime.ye7@gmail.com>
2026-03-17 17:19:11 +0100

    Modify Rooms class to include stages and room selection
    Added 'etape' parameter to the Rooms constructor and implemented 'choisir_salles_quete' method to select rooms based on stages.

--- Rooms.py
+++ Rooms.py
@@ -4,9 +4,10 @@ class Rooms:
 
     salles = []
 
-    def __init__(self, nom, position, taille):
+    def __init__(self, nom, position, taille, etape):
 
         self.nom = nom
+        self.etape = etape
 
         self.zone = Entity(
             model='cube',
@@ -17,6 +18,13 @@ class Rooms:
         )
 
         Rooms.salles.append(self)
+
+    def choisir_salles_quete():
+        """choisit une salle au hasard pour chaque étape (1, 2 et 3)"""
+        salle_e1 = random.choice([s for s in Rooms.salles if s.etape == 1])
+        salle_e2 = random.choice([s for s in Rooms.salles if s.etape == 2])
+        salle_e3 = random.choice([s for s in Rooms.salles if s.etape == 3])
+        return [salle_e1, salle_e2, salle_e3]
 
     @staticmethod  #Définir une méthode sans utiliser l'objet self
     def salle_du_joueur(joueur):
